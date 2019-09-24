3cb46393bf858194bd7375df597879330bdebb11
g-alcorn <griffin.alcorn@gmail.com>
2019-09-24 17:45:51 +0000

    added 1s wait before printing

--- typewriter.js
+++ typewriter.js
@@ -3,15 +3,17 @@ const stdout = process.stdout;
 
 let time = 0;
 let endTime = sentence.length * 50;
-for (const char of sentence) {
-  setTimeout(function() {
-    stdout.write(char);
-  }, time);
-
-  time += 50;
-  if (time === endTime) {
+setTimeout(function() {
+  for (const char of sentence) {
     setTimeout(function() {
-      stdout.write('\n');
-    }, endTime);
+      stdout.write(char);
+    }, time);
+  
+    time += 50;
+    if (time === endTime) {
+      setTimeout(function() {
+        stdout.write('\n');
+      }, endTime);
+    }
   }
-}
+}, 1000);
